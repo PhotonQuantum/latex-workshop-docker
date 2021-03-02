d67b10ae689d4bc33b88909e53bd3082c4b8f9be
PhotonQuantum <self@lightquantum.me>
2021-03-02 19:03:25 +0800

    fix: cannot copy presets to data directory

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -6,7 +6,7 @@ sudo chown -R coder:coder /home/coder
 /usr/local/bin/code-server --user-data-dir /home/coder/vscode_data --install-extension vscodevim.vim
 /usr/local/bin/code-server --user-data-dir /home/coder/vscode_data --install-extension james-yu.latex-workshop
 
-cp -nr /etc/vscode_data /home/coder/vscode_data
+cp -nr /etc/vscode_data/. /home/coder/vscode_data/
 
 bash /usr/local/bin/workshop_fix.sh
 
