b369159d1883d3bca876ba53d7263c921fc0406d
PhotonQuantum <self@lightquantum.me>
2021-03-03 00:42:58 +0800

    fix: use patched latex-workshop

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -4,10 +4,9 @@ set -eu
 sudo chown -R coder:coder /home/coder
 
 /usr/local/bin/code-server --user-data-dir /home/coder/vscode_data --install-extension vscodevim.vim
-/usr/local/bin/code-server --user-data-dir /home/coder/vscode_data --install-extension james-yu.latex-workshop
+
+/usr/local/bin/code-server --user-data-dir /home/coder/vscode_data --install-extension /usr/local/share/latex-workshop.vsix
 
 cp -nr /etc/vscode_data/. /home/coder/vscode_data/
-
-bash /usr/local/bin/workshop_fix.sh
 
 dumb-init /usr/local/bin/code-server --user-data-dir /home/coder/vscode_data "$@"
